d418b2d98fc28891dadc94f528ce26a48c876d20
robbryandev <robbryandev@gmail.com>
2023-01-02 22:37:59 -0800

    Add database variable to home controller

--- templates/csharp/mvc_entity/nameApp/Controllers/HomeController.cs
+++ templates/csharp/mvc_entity/nameApp/Controllers/HomeController.cs
@@ -5,6 +5,12 @@ namespace $name.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly $nameContext _db;
+    public HomeController($nameContext db)
+    {
+      _db = db;
+    }
+    
     [HttpGet("/")]
     public ActionResult Index() {
       return View();
