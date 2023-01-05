3c07149f8046bcd93008c09bedf925965044cc27
robbryandev <robbryandev@gmail.com>
2023-01-04 16:52:34 -0800

    Add blazor_entity csharp template

---
+++ templates/csharp/entity_blazor/nameApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using $name.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace $name
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+      
+      builder.Services.AddServerSideBlazor();
+      builder.Services.AddControllersWithViews();
+
+      builder.Services.AddDbContext<$nameContext>(
+        dbContextOptions => dbContextOptions
+        .UseMySql(
+            builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"])
+        )
+    );
+
+      WebApplication app = builder.Build();
+
+      app.UseDeveloperExceptionPage();
+
+      app.UseStaticFiles();
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseEndpoints(endpoints =>
+      {
+          endpoints.MapControllerRoute(
+              name: "default",
+              pattern: "{controller=Home}/{action=Index}/{id?}");
+          endpoints.MapBlazorHub();
+      });
+
+      app.Run();
+    }
+  }
+}
